4d492d98065a8ea51721092276120aa6bcda3ee0
AleksY22 <71137788+AleksY22@users.noreply.github.com>
2026-01-19 20:33:38 +0300

    final backend

--- server-side/src/main.ts
+++ server-side/src/main.ts
@@ -1,9 +1,18 @@
-/* eslint-disable @typescript-eslint/no-floating-promises */
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './app.module.js';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.use(cookieParser());
+  app.enableCors({
+    origin: [process.env.CLIENT_URL],
+    credentials: true,
+    exposedHeaders: 'set-cookie',
+  });
+
+  await app.listen(process.env.APPLICATION_PORT ?? 5000);
 }
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
 bootstrap();
